9a722681d08c4a83ce84b5abb17712b221f02623
shuling02 <sllin23999@gmail.com>
2026-04-26 22:25:35 -0700

    github page bug fixed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import './css/main.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+    <HashRouter>
+        <App />
+    </HashRouter>
+);
 
 reportWebVitals();
